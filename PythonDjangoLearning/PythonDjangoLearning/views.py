5e519acff5a28b9e236b8b58c9c8210c0b47641d
Anubhav Bhardwaj <anubhav.bhardwaj0222@gmail.com>
2026-09-12 10:34:40 +0530

    added simple html file for landing page

--- PythonDjangoLearning/PythonDjangoLearning/views.py
+++ PythonDjangoLearning/PythonDjangoLearning/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Just a Home page | Landing page")
+    # return HttpResponse("Just a Home page | Landing page")
+    return render(request, 'index.html') # or website/index.html if you choose to put you file in templates/website/
 
 def about(request):
     return HttpResponse("About page")
