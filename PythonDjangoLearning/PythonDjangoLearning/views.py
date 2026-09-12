b1e695d8554caf3a2813c298ed85b401833d1312
Anubhav Bhardwaj <anubhav.bhardwaj0222@gmail.com>
2026-09-12 11:15:51 +0530

    added both page via render | wrote html, css via AI

--- PythonDjangoLearning/PythonDjangoLearning/views.py
+++ PythonDjangoLearning/PythonDjangoLearning/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     # return HttpResponse("Just a Home page | Landing page")
-    return render(request, 'index.html') # or website/index.html if you choose to put you file in templates/website/
+    return render(request, 'home_html/home.html') # or website/index.html if you choose to put you file in templates/website/
 
 def about(request):
-    return HttpResponse("About page")
+    return render(request, 'about_html/about.html')
